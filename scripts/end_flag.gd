840a35144686087fcf2b78f4c55c5ca9f0a391de
RetroRabiul <rabiulcaffe@gmail.com>
2024-04-19 19:17:23 +0600

    Level2

--- scripts/end_flag.gd
+++ scripts/end_flag.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
-
+@export_file("*.tscn") var next_scene
 
 func _on_body_entered(body):
-	pass # Replace with function body.
+	if body.is_in_group("Player"):
+		get_tree().change_scene_to_file(next_scene)
