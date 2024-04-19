cdbfb6690d5f49e173e97b1ecc85db52c7eeff4c
RetroRabiul <rabiulcaffe@gmail.com>
2024-04-19 21:45:33 +0600

    levels

--- scripts/end_flag.gd
+++ scripts/end_flag.gd
@@ -4,4 +4,8 @@ extends Area2D
 
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
-		get_tree().change_scene_to_file(next_scene)
+		call_deferred("change_level")
+		
+
+func change_level():
+	get_tree().change_scene_to_file(next_scene)
