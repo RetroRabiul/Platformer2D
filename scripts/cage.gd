99dc0835a0919cc867c97c4f0c1fceba86913218
RetroRabiul <rabiulcaffe@gmail.com>
2024-04-25 12:39:35 +0600

    Cage,Key

--- scripts/cage.gd
+++ scripts/cage.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends StaticBody2D
 
 var haveKey = false
 
@@ -14,8 +14,8 @@ func _process(delta):
 	pass
 
 
-func _on_body_entered(body):
+func _on_area_2d_body_entered(body):
 	if body.is_in_group("Player"):
 		if haveKey :
-			$Cage.frame += 1
-		
+			$Area2D/Cage.frame += 1
+			print("frame")
